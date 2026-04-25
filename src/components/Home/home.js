b3a8197024565eed568f6df68a4a382c38819a80
CaptSiro <honzahorak433@icloud.com>
2026-04-25 16:15:40 +0200

    Implemented strata special features

---
+++ src/components/Home/home.js
@@ -0,0 +1,12 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
